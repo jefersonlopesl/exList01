7e58a036c8f444ebbe46c006e6b1db033ee8cdeb
Jeferson Lopes <jefersonlopesluis@protonmail.com>
2023-02-02 15:02:03 +0000

    first commit

---
+++ App.java
@@ -0,0 +1,19 @@
+/**
+ * App
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        Tabela salario = new Tabela();
+
+        List<String>lista = new ArrayList<>();
+
+        lista.add("Gimba");
+        lista.add("Constantine");
+
+        System.out.println(salario);
+    }
+}
---
+++ Tabela.java
@@ -0,0 +1,19 @@
+public class Tabela {
+    private double salario=0.0;
+
+
+    public void salario(){
+        
+    }
+
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+
+}
